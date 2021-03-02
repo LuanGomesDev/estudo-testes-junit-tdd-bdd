8c16426949bfd2dd9ecb7ab398018c76c0a2c788
Luan Gomes - Dev CeF <desenvolvimento3@cefpromotora.com.br>
2021-03-02 17:48:31 -0300

    Excluido metodo default na classe de test.

--- src/test/java/br/com/luan/estudotestesjunit/EstudoTestesJunitApplicationTests.java
+++ src/test/java/br/com/luan/estudotestesjunit/EstudoTestesJunitApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.luan.estudotestesjunit;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EstudoTestesJunitApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
